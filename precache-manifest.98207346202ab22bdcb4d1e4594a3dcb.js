410223850527bdc3067c2e5534345355dc1cc3b3
unknown <erichung.94@gmail.com>
2019-04-30 19:38:52 -0500

    Updates

---
+++ precache-manifest.98207346202ab22bdcb4d1e4594a3dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b97cf046642aecd3b3c3f8e1ff442c",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "695ffc07d177a3203df8",
+    "url": "/clicky/static/css/main.439e4f1a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "695ffc07d177a3203df8",
+    "url": "/clicky/static/js/main.0163967e.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
